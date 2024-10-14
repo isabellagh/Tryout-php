b0b3ef462ccc6b7d121bfa8f70f45fc816a5c5d3
Isabella Henriques <isabellagh88@gmail.com>
2024-10-13 22:01:48 -0400

    helpers and partials

--- helpers.php
+++ helpers.php
@@ -10,3 +10,35 @@
  function basePath($path = '') {
     return __DIR__. '/' . $path;
  }
+
+ /**
+  * Load a View
+  *
+  * @param string $name
+  * @return void
+  */
+  function loadView($name) {
+   $viewPath = basePath("views/{$name}.view.php");
+
+   if (file_exists($viewPath)) {
+      require $viewPath;
+   } else {
+      echo "View '{$name} not found!'";
+   }
+  }
+
+  /**
+  * Load a Partial
+  *
+  * @param string $name
+  * @return void
+  */
+  function loadPartial($name) {
+   $partialPath = basePath("views/partials/{$name}.php");
+
+   if (file_exists($partialPath)) {
+      require $partialPath;
+   } else {
+      echo "partial '{$name} not found!'";
+   }
+  }
