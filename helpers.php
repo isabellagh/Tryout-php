86aa36315d8ff6f4dca991479c94ebbb698f024b
Isabella Henriques <isabellagh88@gmail.com>
2024-10-13 23:35:05 -0400

    routes and views rendering

--- helpers.php
+++ helpers.php
@@ -42,3 +42,29 @@
       echo "partial '{$name} not found!'";
    }
   }
+
+
+  /**
+  * Inspect a value(s)
+  *
+  * @param mixed $value
+  * @return void
+  */
+    function inspect($value) {
+      echo '<pre>';
+      var_dump($value);
+      echo '</pre>';
+    }
+
+
+/**
+  * Inspect a value(s) and die
+  *
+  * @param mixed $value
+  * @return void
+  */
+  function inspectAndDie($value) {
+   echo '<pre>';
+   die(var_dump($value));
+   echo '</pre>';
+ }
